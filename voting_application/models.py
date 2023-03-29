3763b1fccf7e62fa7c36e793a95a53fd0bf80b34
Alina <alinatussupova@gmail.com>
2023-03-29 08:51:27 +0600

    Initial Commit.

---
+++ voting_application/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Designing database model
+class Form(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField()
+    date = models.DateField()
+    choice = models.CharField(max_length=80)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
